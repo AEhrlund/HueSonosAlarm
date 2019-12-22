ff19ef5fee9adfb282cb829df9c9d01628720e3b
Alf Ehrlund <alfen99@hotmail.com>
2019-12-22 18:44:44 +0700

    Added empty unit test

---
+++ frontend/tests/unit/example.spec.js
@@ -0,0 +1,11 @@
+// import { expect } from 'chai'
+// import { shallowMount } from '@vue/test-utils'
+// import TheHueSonosAlarm from '@/components/TheHueSonosAlarm.vue'
+
+describe('TheHueSonosAlarm.vue', () => {
+  it('Test1', () => {
+    // const wrapper = shallowMount(TheHueSonosAlarm, {
+    // })
+    // expect(true).to.be(true)
+  })
+})
